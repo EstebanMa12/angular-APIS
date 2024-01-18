f2adf1036c8f4aa83a3a3ded9195fc09f392bce8
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-18 14:30:50 -0500

    Add HttpClient parameters and retry logic

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 import { CreateProductDTO, Product } from './../models/product.model';
+import { retry } from 'rxjs/operators';
+import { pipe } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,8 +16,16 @@ export class ProductsService {
     private http: HttpClient
   ) { }
 
-  getAll() {
-    return this.http.get<Product[]>(this.apiUrl);
+  getAll(limit?:number, offset?:number) {
+    let params = new HttpParams();
+    if(limit && offset){
+      params = params.set('limit', limit);
+      params = params.set('offset', offset);
+    }
+    return this.http.get<Product[]>(this.apiUrl, {params})
+    .pipe(
+      retry(3)
+    )
   }
 
   get(id: string) {
