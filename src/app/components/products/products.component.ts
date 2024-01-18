ec0f77969ebb0e5f4714348eabde0ae2cf0249c7
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-18 09:56:45 -0500

    Update dependencies and product detail component

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -16,6 +16,17 @@ export class ProductsComponent implements OnInit {
   total = 0;
   products: Product[] = [];
   showProductDetail = false;
+  productChosen: Product = {
+    id: '',
+    price: 0,
+    images: [],
+    title: '',
+    category: {
+      id:'',
+      name:'',
+    },
+    description: ''
+  }
 
   constructor(
     private storeService: StoreService,
@@ -44,8 +55,8 @@ export class ProductsComponent implements OnInit {
     this.toggleProductDetail();
     this.productsService.getProduct(id)
     .subscribe(data => {
-      console.log(data);
-
+      // this.toggleProductDetail();
+      this.productChosen = data;
     });
   }
 
