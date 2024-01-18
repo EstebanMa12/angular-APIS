1c0fedf559e5b6be9bfff86e828b1638acf04409
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-18 16:54:19 -0500

    Fix currency formatting and add taxes calculation

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 
 import { CreateProductDTO, Product } from './../models/product.model';
-import { catchError, retry } from 'rxjs/operators';
+import { catchError, retry, map } from 'rxjs/operators';
 import { pipe, throwError } from 'rxjs';
 
 @Injectable({
@@ -24,7 +24,13 @@ export class ProductsService {
     }
     return this.http.get<Product[]>(this.apiUrl, {params})
     .pipe(
-      retry(3)
+      retry(3),
+      map(products=> products.map(item=>{
+        return{
+          ...item,
+          taxes: item.price*0.19
+        }
+      }))
     )
   }
 
